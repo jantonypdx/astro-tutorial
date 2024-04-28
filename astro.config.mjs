2824064940e203d4c9ba10c476f84462fcb6f380
John Antony <jantony_pdx@yahoo.com>
2024-04-28 12:59:09 -0700

    Start tutorial over

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
-import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://astro-tutorial-bcc2f0.netlify.app/",
-  integrations: [preact()]
-});
+export default defineConfig({});
